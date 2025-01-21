4f14e93a763865c1c84c1734159fbf3428df32c7
Kennedy <kennedyfigueiredodelima@gmail.com>
2025-01-20 22:54:59 -0300

    teste erro-email-deploy

--- my_flask_app/app/main.py
+++ my_flask_app/app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)  # Instância da aplicação Flask
 
 @app.route("/")  # Rota principal da aplicação
 def home():
-    returqn "<h1>Hello, NTT DATA</h1>"  # Retorna uma resposta HTML para o navegador
+    return "<h1>Hello, NTT DATA</h1>"  # Retorna uma resposta HTML para o navegador
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)  # Executa o servidor
